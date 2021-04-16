da400e8931d050ab5f3adcd5203cd221153e2832
jatinchopra <jatinjc99@gmail.com>
2021-04-16 21:14:47 +0530

    showed items from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,46 @@
+// import { format } from 'prettier';
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
+import { apiGet } from '../misc/config';
+
 const Home = () => {
   const [input, setInput] = useState('');
-  const onInputChange = ev => {
-    setInput(ev.target.value);
-  };
+  const [results, setResults] = useState(null);
 
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=men
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
 
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+  const onInputChange = ev => {
+    setInput(ev.target.value);
   };
 
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div> no results </div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {' '}
+          {results.map(item => (
+            <div key={item.show.id}> {item.show.name}</div>
+          ))}{' '}
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -35,6 +55,7 @@ const Home = () => {
         {' '}
         search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
