51ff1a1cd964b22d0b69a42f2c32091ae943aaac
jatinchopra <jatinjc99@gmail.com>
2021-04-16 17:54:38 +0530

    edit nav bar and created page components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        this is homepage
-      </Route>
-      <Route exact path="/starred">
-        This is home page
-      </Route>
-      <Route>Not Found 404</Route>
-    </Switch>
+    <div>
+      <Navs> </Navs>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>Not Found 404</Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>this is sdsdhome</div>;
+};
+
+export default Home;
